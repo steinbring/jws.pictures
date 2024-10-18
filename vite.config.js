b6915ec77824deaf705ad7c8917add836c1d39a7
Joe Steinbring <joe@jws.app>
2024-10-17 19:29:23 -0500

    Trying to get routing working right

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: 'https://jws.pictures/',
+  base: '/',
   plugins: [vue()],
   root: './src',  // Set the root folder for source files
   build: {
